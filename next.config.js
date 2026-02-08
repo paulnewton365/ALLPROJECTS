097f0d61e56e9fa17f96f42f2e0661aceb2a2e90
paulnewton365 <paulnewton365@gmail.com>
2026-02-08 15:00:55 -0500

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -10,5 +10,16 @@ const nextConfig = {
     BUILD_TIME: new Date().toISOString(),
     BUILD_COMMIT: (process.env.VERCEL_GIT_COMMIT_SHA || "local").slice(0, 7),
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "X-Frame-Options", value: "ALLOWALL" },
+          { key: "Content-Security-Policy", value: "frame-ancestors *" },
+        ],
+      },
+    ];
+  },
 }
 module.exports = nextConfig
